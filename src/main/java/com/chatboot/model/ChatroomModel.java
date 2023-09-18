ae482bf5d704962668c8824d629a69057b83669b
jawad <jawad.rafiq@starzplayarabia.com>
2023-09-18 23:08:42 +0500

    Update the h2 databases things

--- src/main/java/com/chatboot/model/ChatroomModel.java
+++ src/main/java/com/chatboot/model/ChatroomModel.java
@@ -4,10 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.security.Timestamp;
 
 @Entity
 public class ChatroomModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String username;
+    @CreationTimestamp
+    private Timestamp jointime;
+
 }
--- src/main/java/com/chatboot/model/MessageModel.java
+++ src/main/java/com/chatboot/model/MessageModel.java
@@ -1,13 +1,25 @@
 package com.chatboot.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.chatboot.controller.request.MESSAGETYPE;
+import jakarta.persistence.*;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.security.Timestamp;
 
 @Entity
 public class MessageModel {
     @Id
+    @Column(name = "message_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name = "message_content")
+    private String content;
+    @Column(name = "message_sender")
+    private String sender;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "message_messageType")
+    private MESSAGETYPE messagetype;
+    @CreationTimestamp
+    private Timestamp sendedAt;
 }
